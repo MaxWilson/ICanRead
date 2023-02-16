a439b341918cfeb9f145089b1634fb7fc698cba9
Max Wilson <wilson.max@gmail.com>
2023-02-15 19:06:36 -0800

    Improve speech patterns a bit, allow user to interrupt. Also add a Say It Again button.

--- src/Home.fs
+++ src/Home.fs
@@ -23,6 +23,8 @@ type Model = {
     }
 
 type Msg =
+    | VerbalizeProblem
+    | Interrupt
     | Pick of word: string
     | HelpLetter of letter: string
 
@@ -34,31 +36,39 @@ type Msg =
 }""")>]
 let private speak : double -> string -> unit = jsNative
 
+[<Emit("""rate => msg => {
+    window.speechSynthesis.cancel();
+    var msg = new SpeechSynthesisUtterance(msg);
+    msg.pitch = 3
+    msg.rate = rate;
+    window.speechSynthesis.speak(msg)
+}""")>]
+let private interrupt : double -> string -> unit = jsNative
+
 let update msg model =
     match msg with
     | Pick word ->
         let game = GameLogic.update model.game word
-        match game.problem.answer with
+        speak 1. (game.feedback)
+        speak 1. $" Now, which button says '{game.problem.answer}'?"
+        { model with game = game }, []
+    | HelpLetter letter ->
+        interrupt 0.5 letter
+        model, Cmd.Empty
+    | VerbalizeProblem ->
+        match model.game.problem.answer with
         | "" -> ()
         | v ->
-            speak 1. (game.feedback + $" Now... which word says '{v}'.")
-        { model with game = game }
-    | HelpLetter letter ->
-        speak 0.5 letter
-        model
+            interrupt 1. $" Which button says '{v}'?"
+        model, Cmd.Empty
 
 type NavCmd = Fresh
 
 let init _ =
-    let game = GameLogic.init()
-    match game.problem.answer with
-    | "" -> ()
-    | v ->
-        speak 1. ("Which word says " + v)
     {
         userName = "<UserName>"
-        game = game
-        }
+        game = GameLogic.init()
+        }, Cmd.ofMsg VerbalizeProblem
 
 let class' (className: string) ctor (elements: _ seq) = ctor [prop.className className; prop.children elements]
 let classTxt' (className: string) ctor (txt: string) = ctor [prop.className className; prop.text txt]
@@ -88,6 +98,8 @@ let view model dispatch =
                         ]
                 ]
 
+
+            Html.button [prop.text "Say it again"; prop.onClick (thunk1 dispatch VerbalizeProblem)]
             if model.game.feedback <> "" then
                 Html.div model.game.feedback
 
@@ -110,6 +122,4 @@ module Nav =
             Fresh
         | _ -> Fresh
     let nav (navCmd: NavCmd) model =
-        init navCmd, Cmd.Empty
-
-open Elmish.React
+        init navCmd
--- src/Main.fs
+++ src/Main.fs
@@ -5,7 +5,7 @@ open Elmish.Navigation
 open Elmish.React
 open Home
 
-Program.mkSimple init update view
+Program.mkProgram init update view
     |> Program.toNavigable Nav.parse Nav.nav
     |> Program.withReactBatched "root"
     |> Program.run
