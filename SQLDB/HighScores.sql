4dcc5531402d9d0a5582ce59cada10ed456ff8a9
Max Wilson <wilson.max@gmail.com>
2023-02-25 21:38:41 -0800

    Convert CosmosDB backend to SQL

---
+++ SQLDB/HighScores.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE ICanRead.[HighScore]
+(
+	[Id] INT IDENTITY NOT NULL PRIMARY KEY,
+	[Name] NVARCHAR(100) NULL,
+	[Score] INT NOT NULL,
+	[Date] DATETIME NOT NULL Default (sysdatetimeoffset())
+)
+GO
+Create index IX_ByScoreAndName on ICanRead.HighScore(Score, Name)
+GO
+Create index IX_ByDateScoreAndName on ICanRead.HighScore(Date, Score, Name)
---
+++ SQLDB/Login1.sql
@@ -0,0 +1,3 @@
+﻿--create login [ICanRead] with password = 'ICanRead_changethis' must_change, default_database = [ICanRead], default_language = [us_english]
+--go
+--create user [ICanRead] for login [ICanRead]
